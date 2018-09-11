ad1aa0a21a5bbe0654d0545cb946cb7c35fb6c8f
Mikhail B. Nunez <mikhailbnunez@gmail.com>
2018-09-11 08:53:44 +0800

    Schema Converter

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,23 @@
 
-function popMessage() {
-    alert("Hi!!");
+function convert() {
+    var source = document.getElementById("sourceInput").value;
+
+    var output = source.split("\n")
+                       .map(s => s.trim())
+                       .map(s => extractFirstWord(s))
+                       .map(s => convertToSchemaFormat(s))
+                       .join("\n");
+
+    document.getElementById("targetOutput").value = output;
+}
+
+function extractFirstWord(str) {
+    return str.split(" ")[0];
+}
+
+function convertToSchemaFormat(str) {
+    return '{' + '\n' +
+    '  "name": "' + str + '",' + '\n' +
+    '  "type": [ "null", "string" ]' + '\n' +
+    '},';
 }
