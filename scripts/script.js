55ea9ac8a707d081b80d3c2e4cce1d9b960543ee
Mikhail B. Nunez <mikhailbnunez@gmail.com>
2018-09-11 10:16:50 +0800

    Completed schema output

--- scripts/script.js
+++ scripts/script.js
@@ -2,13 +2,22 @@
 function convert() {
     var source = document.getElementById("sourceInput").value;
 
+    var header = '{' + '\n'
+        + '  "type": "record",' + '\n'
+        + '  "name": "",' + '\n'
+        + '  "namespace": "",' + '\n'
+        + '  "fields": [' + '\n';
+
+    var bottom = '  ]' + '\n'
+        + '}';
+
     var output = source.split("\n")
                        .map(s => s.trim())
                        .map(s => extractFirstWord(s))
                        .map(s => convertToSchemaFormat(s))
                        .join("\n");
 
-    document.getElementById("targetOutput").value = removeLastComma(output);
+    document.getElementById("targetOutput").value = header + removeLastComma(output) + '\n' + bottom;
 }
 
 function extractFirstWord(str) {
@@ -16,14 +25,14 @@ function extractFirstWord(str) {
 }
 
 function convertToSchemaFormat(str) {
-    return '{' + '\n' +
-    '  "name": "' + str + '",' + '\n' +
-    '  "type": [ "null", "string" ]' + '\n' +
-    '},';
+    return '    {' + '\n' +
+    '      "name": "' + str + '",' + '\n' +
+    '      "type": [ "null", "string" ]' + '\n' +
+    '    },';
 }
 
 function removeLastComma(str) {
-    if (str.charAt( str.length - 1) == ",") {
+    if (str.charAt(str.length - 1) == ",") {
         return str.slice(0, -1);
     }
 }
